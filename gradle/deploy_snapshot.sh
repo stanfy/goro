a67bb89532eeb14f2218dbc775a6a873a8b2c221
Roman Mazur <rmazur@stanfy.com.ua>
2015-07-28 11:48:56 +0300

    Add snapshot deploy configuration

---
+++ gradle/deploy_snapshot.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+SLUG="stanfy/goro"
+JDK="oraclejdk8"
+BRANCH="master"
+
+set -e
+
+if [ "$TRAVIS_REPO_SLUG" != "$SLUG" ]; then
+  echo "Skipping snapshot deployment: wrong repository. Expected '$SLUG' but was '$TRAVIS_REPO_SLUG'."
+elif [ "$TRAVIS_JDK_VERSION" != "$JDK" ]; then
+  echo "Skipping snapshot deployment: wrong JDK. Expected '$JDK' but was '$TRAVIS_JDK_VERSION'."
+elif [ "$TRAVIS_PULL_REQUEST" != "false" ]; then
+  echo "Skipping snapshot deployment: was pull request."
+elif [ "$TRAVIS_BRANCH" != "$BRANCH" ]; then
+  echo "Skipping snapshot deployment: wrong branch. Expected '$BRANCH' but was '$TRAVIS_BRANCH'."
+else
+  echo "Deploying snapshot..."
+  ./gradlew clean uploadArchives -PnexusUsername=$NEXUS_USER -PnexusPassword=$NEXUS_PASSWORD
+  echo "Snapshot deployed!"
+fi
